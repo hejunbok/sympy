9df7d065a454272afa6c1d2b731c9e98f842a262
jgoppert <james.goppert@gmail.com>
2012-09-29 00:34:01 -0400

    Added more complicated test of matrix derivative.

--- examples/advanced/matrix_deriv.py
+++ examples/advanced/matrix_deriv.py
@@ -2,6 +2,12 @@
 from sympy import *
 
 A = MatrixSymbol('A',3,3)
+C = MatrixSymbol('C',3,3)
+D = MatrixSymbol('D',3,3)
 x = MatrixSymbol('x',3,1)
+b = MatrixSymbol('b',3,1)
+e = MatrixSymbol('e',3,1)
 
 pprint(diff(x.T*A*x,x).fix(x))
+a = (A*x+b).T*C*(D*x+e)
+print diff(a,x).fix(x)
